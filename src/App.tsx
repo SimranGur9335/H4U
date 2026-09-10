ed8bd547d7d09319698c0b7f518d670c47d57e3e
SimranGur9335 <simran.gur@example.com>
2026-09-10 23:23:47 +0530

    Update H4U complete code

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes/AppRoutes';
 import { ThemeProvider } from './context/ThemeContext';
+import { AuthProvider } from './context/AuthContext';
 
 export const App: React.FC = () => {
   return (
-    <ThemeProvider>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </ThemeProvider>
+    </AuthProvider>
   );
 };
 
